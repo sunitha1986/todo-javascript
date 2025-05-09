372e948c92fb155909df1b176aa04bbd303ce394
sunitha1986 <sunitha.gbtech@gmail.com>
2025-05-09 10:08:57 +0530

    added logic

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("In Script");
+
+const myNodeList = document.getElementsByTagName("li");
+for (let i = 0; i < myNodeList.length; i++) {
+  addDeleteBtn(myNodeList[i]);
+}
+
+function onClickDeleteBtn() {
+  const close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      const div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+function addDeleteBtn(node) {
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  node.appendChild(span);
+  onClickDeleteBtn();
+}
+
+const addNewTodo = () => {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("todo-input").value;
+  const textNode = document.createTextNode(inputValue);
+  li.appendChild(textNode);
+  if (inputValue == "") {
+    alert("Enter input ");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("todo-input").value = "";
+
+  addDeleteBtn(li);
+
+  onClickDeleteBtn();
+};
